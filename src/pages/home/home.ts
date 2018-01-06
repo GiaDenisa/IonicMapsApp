63928d6d269da8c137439e956df1732a6ef2316a
GiaDenisa <georgiana.anghel24@yahoo.com>
2018-01-06 13:02:51 +0200

    Bucharest coordinates in home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {} from '@types/googlemaps';
 
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,24 +11,32 @@ import {} from '@types/googlemaps';
 export class HomePage {
 
   constructor(public navCtrl: NavController) 
-  {
-    console.log("Am ajuns in constructorul de pe pagina home");
+  
+{ 
+console.log("Am ajuns in constructorul de pe pagina home");
   }
- 
+
+
+
+
   ngAfterViewInit() {
 
-  	let map = document.getElementById('map');
-  	console.log(map);
-  	this.initMap(map);
+    let map = document.getElementById('map');
+    console.log(map);
+    this.initMap(map);
   }
 
   initMap(mapCanvas)
   {
-  	var mapOptions = {
-  		center: new google.maps.LatLng(-34.397, 150.644),
-  		zoom: 8
-  	};
-  	let map = new google.maps.Map(mapCanvas, mapOptions);
+    var mapOptions = {
+      center: new google.maps.LatLng(44.439663, 26.096306),
+      zoom: 12
+    };
+    let map = new google.maps.Map(mapCanvas, mapOptions);
   }
 
-}
+   }
+
+
+  
+
